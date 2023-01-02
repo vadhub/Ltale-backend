6125f070c15f69986458078e99045306097e41d4
vadhub <gabderahmanov99@gmail.com>
2023-01-02 20:33:20 +0300

    search by user id

--- ltale/src/main/java/com/vad/ltale/dao/MessageRepository.java
+++ ltale/src/main/java/com/vad/ltale/dao/MessageRepository.java
@@ -3,6 +3,9 @@ package com.vad.ltale.dao;
 import com.vad.ltale.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface MessageRepository extends JpaRepository<Message, Integer> {
+    List<Message> findAllByUserId(int userId);
 }
