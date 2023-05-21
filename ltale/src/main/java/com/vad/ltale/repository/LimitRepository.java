c71ea351d59dfdc190c09ea9b5687f05971e6cdb
vadim <gabderahmanov99@gmail.com>
2023-05-21 20:02:44 +0300

    get limit by user id

--- ltale/src/main/java/com/vad/ltale/repository/LimitRepository.java
+++ ltale/src/main/java/com/vad/ltale/repository/LimitRepository.java
@@ -2,6 +2,15 @@ package com.vad.ltale.repository;
 
 import com.vad.ltale.entity.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
+@RepositoryRestResource(excerptProjection = InlineLimit.class)
 public interface LimitRepository extends JpaRepository<Limit, Long> {
+
+    @Query("FROM Limit l WHERE l.userId= :userId")
+    Optional<Limit> limitByUserId(@Param("userId") Long userId);
 }
