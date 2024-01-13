d415032a6a916878f07b4956b7f824296c04bd2a
vadim <gabderahmanov99@gmail.com>
2024-01-13 16:55:13 +0300

    conflict http if user already exist on registration

--- ltale/src/main/java/com/vad/ltale/controller/MainController.java
+++ ltale/src/main/java/com/vad/ltale/controller/MainController.java
@@ -4,6 +4,8 @@ import com.vad.ltale.entity.User;
 import com.vad.ltale.entity.UserRequest;
 import com.vad.ltale.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,8 +25,14 @@ public class MainController {
 
     @ResponseBody
     @PostMapping("/registration")
-    public User registrationPage(@RequestBody UserRequest user) {
-        return registrationService.save(user);
+    public ResponseEntity<Object> registrationPage(@RequestBody UserRequest user) {
+        User u = null;
+        try {
+            u = registrationService.save(user);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+        return ResponseEntity.ok(u);
     }
 
     @ResponseBody
